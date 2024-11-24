2fc8ddb431ab6dc2f00d13df69e67a66937d13f1
Michael Wu <mw10013@gmail.com>
2024-11-24 12:31:16 -0500

    prettier

--- migrations/0001_init.sql
+++ migrations/0001_init.sql
@@ -1,1 +1,5 @@
 -- Migration number: 0001 	 2024-11-24T16:28:45.449Z
+create table roles (role_id text primary key)
+--> statement-breakpoint
+create table membership_roles (membership_role_id text primary key)
+--> statement-breakpoint
---
+++ prettier.config.mjs
@@ -0,0 +1,52 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: false,
+  singleQuote: true,
+  bracketSameLine: true,
+  bracketSpacing: true,
+  arrowParens: "always",
+  trailingComma: "es5",
+
+  // https://github.com/IanVS/prettier-plugin-sort-imports
+  importOrder: [
+    "<TYPES>^(node:)",
+    "<TYPES>",
+    "<TYPES>^[.]",
+    "^(react/(.*)$)|^(react$)",
+    "<THIRD_PARTY_MODULES>",
+    "^~/(.*)$",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.3.3",
+
+  // https://github.com/prettier/prettier-vscode/issues/3248: "prettier.documentSelectors": ["**/*.sql"]
+  // https://github.com/tailwindlabs/prettier-plugin-tailwindcss
+  // https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/59
+  tailwindConfig: "./functions/app/tailwind.config.ts",
+  plugins: [
+    "prettier-plugin-sql",
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  overrides: [
+    {
+      files: ["*.sql"],
+      // https://github.com/un-ts/prettier/tree/master/packages/sql#parser-options
+      // https://github.com/sql-formatter-org/sql-formatter/tree/master/docs
+      options: {
+        // https://github.com/sql-formatter-org/sql-formatter/blob/master/docs/language.md#impact-on-bundle-size
+        language: "sqlite",
+        dialect: "sqlite",
+        keywordCase: "lower"
+      },
+    },
+  ],
+};
+
+export default config;
