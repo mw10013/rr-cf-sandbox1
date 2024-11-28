031a2d2ad52f3b1d7532728a6fe1375eb3720d61
Michael Wu <mw10013@gmail.com>
2024-11-27 20:42:01 -0500

    schema

--- app/routes/d1.tsx
+++ app/routes/d1.tsx
@@ -4,6 +4,50 @@ import { invariant } from '@epic-web/invariant'
 import { Button } from '~/lib/components/rac-starter/Button'
 import { Form } from '~/lib/components/rac-starter/Form'
 import { TextField } from '~/lib/components/rac-starter/TextField'
+import { Schema } from "effect"
+
+const RoleSchema = Schema.Literal("admin", "customer")
+export type Role = typeof RoleSchema.Type
+export const roles = RoleSchema.literals
+export const isRole = Schema.is(RoleSchema)
+
+export const assertsRole: Schema.Schema.ToAsserts<typeof RoleSchema> = Schema.asserts(RoleSchema)
+
+const MembershipRoleSchema = Schema.Literal("owner", "member")
+export type MembershipRole = typeof MembershipRoleSchema.Type
+export const membershipRoles = MembershipRoleSchema.literals
+export const isMembershipRole = Schema.is(MembershipRoleSchema)
+
+export const assertsMembershipRole: Schema.Schema.ToAsserts<typeof MembershipRoleSchema> = Schema.asserts(
+  MembershipRoleSchema
+)
+
+export class User extends Schema.Class<User>("User")({
+  userId: Schema.Number,
+  email: Schema.String,
+  name: Schema.String,
+  role: RoleSchema
+}) {}
+
+export class Organization extends Schema.Class<Organization>("Organization")({
+  organizationId: Schema.Number,
+  name: Schema.String
+}) {}
+
+export class Membership extends Schema.Class<Membership>("Membership")({
+  organizationId: Schema.Number,
+  userId: Schema.Number,
+  membershipRole: MembershipRoleSchema
+}) {}
+
+export class MembershipAggregate extends Membership.extend<MembershipAggregate>("MembershipAggregate")({
+  userEmail: Schema.String,
+  userName: Schema.String
+}) {}
+
+export class OrganizationAggregate extends Organization.extend<OrganizationAggregate>("OrganizationAggregate")({
+  memberships: Schema.Array(MembershipAggregate)
+}) {}
 
 export async function loader({ context }: Route.LoaderArgs) {
   const data = await context.cloudflare.env.D1.prepare('select * from users')
@@ -19,7 +63,7 @@ export async function action({ request, context }: Route.ActionArgs) {
   console.log({ intent })
 
   switch (intent) {
-    case 'form_1': {
+    case 'user_create': {
       const email = String(formData.get('email'))
       invariant(typeof email === 'string' && email, 'Missing email')
       console.log({ email })
@@ -34,8 +78,8 @@ export default function RouteComponent({ loaderData }: Route.ComponentProps) {
     <div className="container p-6">
       <Form method="post" className="max-w-sm">
         <TextField name="email" label="Email" />
-        <Button type="submit" name="intent" value="form_1">
-          form_1
+        <Button type="submit" name="intent" value="user_create">
+          user_create
         </Button>
       </Form>
       <pre>{JSON.stringify(loaderData.data, null, 2)}</pre>
