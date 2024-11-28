c3169114355ff5cebaed0ec50ecd2b77fe2bee66
Michael Wu <mw10013@gmail.com>
2024-11-28 17:47:36 -0500

    schema

--- app/Application.ts
+++ app/Application.ts
@@ -1,4 +1,9 @@
 import { Schema } from 'effect'
+import { Trim } from 'effect/Schema'
+
+
+/// option from non-empty trimmed string
+// https://github.com/Effect-TS/effect/issues/3335
 
 // https://www.typescript-validators.com/effect-schema
 // https://github.com/dearlordylord/parsers-jamboree/blob/master/libs/effect-ts-schema/src/lib/effect-ts-schema.ts
@@ -19,4 +24,18 @@ import { Schema } from 'effect'
 //   )
 // ).pipe(Schema.brand(EmailBrand))
 
-export const EmailSchema = Schema.NonEmptyString
+const EmailBrand = Symbol.for('Email')
+
+// no built-in email combinator by-design (lot of definitions out there)
+// const EmailSchema = Schema.NonEmptyTrimmedString.pipe(
+//   Schema.pattern(
+//     /^(?!\.)(?!.*\.\.)([A-Z0-9_+-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i
+//   )
+// )
+
+// export const EmailSchema = Schema.NonEmptyString
+// export const EmailSchema = Schema.NonEmptyTrimmedString
+export const EmailSchema = Schema.NonEmptyTrimmedString.pipe(Schema.brand(EmailBrand))
+// export const EmailSchema = Trim
+// export const EmailSchema = Schema.compose(Trim, Schema.nonEmptyString)
+// export const EmailSchema = Schema.compose(Schema.nonEmptyString, Trim)
--- test/Application.test.ts
+++ test/Application.test.ts
@@ -1,20 +1,18 @@
 import { assert, describe, expect, it } from '@effect/vitest'
 import { Schema } from 'effect'
-import { EmailSchema } from '../app/Application'
 import { ParseError } from 'effect/ParseResult'
+import { EmailSchema } from '../app/Application'
 
 describe('Domain', () => {
   it('should handle email', () => {
     const email = Schema.decodeUnknownSync(EmailSchema)('a@mail.com')
-    assert.equal(
-      Schema.decodeUnknownSync(EmailSchema)('a@mail.com'),
-      'a@mail.com'
+    // console.log({ email: Schema.decodeUnknownSync(EmailSchema)(' a@mail.com ')})
+    expect(() => Schema.decodeUnknownSync(EmailSchema)('')).toThrowError(
+      ParseError
     )
-    expect(Schema.decodeUnknownSync(EmailSchema)('a@mail.com')).toBe(
-      'a@mail.com'
-    )
-    expect(() => Schema.decodeUnknownSync(EmailSchema)('')).toThrowError(ParseError)
-    console.log({ email })
+    expect(() =>
+      Schema.decodeUnknownSync(EmailSchema)('a@mail.com ')
+    ).toThrowError(ParseError)
     expect(true).toBe(true)
   })
 })
