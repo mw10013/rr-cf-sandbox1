048726a606a3bd03103ccb9bbc390e5a5b779f11
Michael Wu <mw10013@gmail.com>
2024-12-05 20:48:42 -0500

    .

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,16 @@
 import type { Route } from './+types/_index'
-import { Button } from '~/lib/components/rac-starter/Button'
-// import { Form } from '~/lib/components/rac-starter/Form'
-import { TextField } from '~/lib/components/rac-starter/TextField'
-import { Welcome } from '../welcome/welcome'
+import { TableBody } from 'react-aria-components'
 import { Form } from 'react-router'
+import { Button } from '~/lib/components/rac-starter/Button'
+import {
+  Cell,
+  Column,
+  Row,
+  Table,
+  TableHeader,
+} from '~/lib/components/rac-starter/Table'
+
+// import { Form } from '~/lib/components/rac-starter/Form'
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,14 +19,34 @@ export function meta({}: Route.MetaArgs) {
   ]
 }
 
-export function loader({ context }: Route.LoaderArgs) {
-  return null
+export async function loader({ context }: Route.LoaderArgs) {
+  const d1 = context.cloudflare.env.D1
+  const result = await d1.prepare('select * from users').run()
+  return { result }
 }
 
 export async function action({ request, context }: Route.ActionArgs) {
   const formData = await request.formData()
   const intent = String(formData.get('intent'))
   console.log({ intent })
+  switch (intent) {
+    case 'seed':
+      {
+        const d1 = context.cloudflare.env.D1
+        await d1.prepare('delete from users').run()
+        const result = await d1
+          .prepare(
+            `insert into users (email, name, role) values 
+          ('admin@mail.com', 'admin', 'admin'),
+          ('a@mail.com', 'a', 'customer')`
+          )
+          .run()
+        console.log({ result })
+      }
+      break
+    default:
+      throw new Error(`Unsupported intent: ${intent}`)
+  }
   return null
 }
 
@@ -28,9 +55,24 @@ export default function RouteComponent({ loaderData }: Route.ComponentProps) {
     <div className="containner p-6">
       <Form method="post" className="max-w-sm">
         <Button type="submit" name="intent" value="seed">
-          seed
+          Seed
         </Button>
       </Form>
+      <Table>
+        <TableHeader>
+          <Column isRowHeader>ID</Column>
+          <Column>Email</Column>
+          <Column>Name</Column>
+        </TableHeader>
+        <TableBody>
+          <Row>
+            <Cell>1</Cell>
+            <Cell>admin@mail.com</Cell>
+            <Cell>admin</Cell>
+          </Row>
+        </TableBody>
+      </Table>
+      <pre>{JSON.stringify({ loaderData }, null, 2)}</pre>
     </div>
   )
 }
